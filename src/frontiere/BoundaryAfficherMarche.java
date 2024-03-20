0fa51a99d1de1a8e4a28df698fd9e979f5635ef1
Emmatho <ematho6014@gmail.com>
2024-03-20 22:45:24 +0100

    Méthode afficherMarche complète dans la classe BoundaryAfficherMarche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,22 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			int i = 0;
+			System.out.println(nomAcheteur + ", vous trouverez au marché :");
+			while (i < infosMarche.length) {
+				StringBuilder infosEtal = new StringBuilder();
+				infosEtal.append("-" + infosMarche[i]);
+				i++;
+				infosEtal.append(" qui vend " + infosMarche[i]);
+				i++;
+				infosEtal.append(" " + infosMarche[i]);
+				i++;
+				System.out.println(infosEtal.toString());
+			}
+		}
 	}
 }
