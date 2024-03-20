af581458dbcd197042d15c75b10deadaba5f794d
Emmatho <ematho6014@gmail.com>
2024-03-20 11:49:06 +0100

    Complétion de la méthode trouverEtalVendeur dans la classe ControlTrouverEtalVendeur

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -1,5 +1,6 @@
 package controleur;
 
+
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -11,8 +12,10 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
 		Etal etal = null;
+		if (village.trouverHabitant(nomVendeur) != null) {
+			etal = village.rechercherEtal(village.trouverHabitant(nomVendeur));
+		}
 		return etal;
 	}
 }
